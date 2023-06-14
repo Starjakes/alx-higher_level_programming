3e7619cf920f8f654a0050add2bb632e2234155f
Starjakes <amehjacobe@gmail.com>
2023-06-14 12:30:57 -0700

    Roman to Integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "DM": 900, "M": 1000,}
+    roman = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "DM": 900, "M": 1000}
     if not isinstance(roman_string, str):
         return 0
     a = 0
