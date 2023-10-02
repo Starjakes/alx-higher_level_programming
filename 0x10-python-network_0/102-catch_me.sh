502a2e5ee9a57bf5bfe0148876ffd0ddf3f79880
starjakes <starjakese@gmail.com>
2023-10-01 20:54:08 -0700

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-#  send a post a requst with json file
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+
+# Set the target URL
+url="http://0.0.0.0:5000/catch_me"
+
+# Send a POST request with a custom header
+curl -X POST "$url" -H "Custom-Header: You got me!" >/dev/null 2>&1
+
+# The server response will be displayed by curl, so we don't need to echo anything
