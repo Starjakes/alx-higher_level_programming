8dc1c92ce733dcba78bb7dcaafa5115652eabf88
starjakes <starjakese@gmail.com>
2023-10-01 21:14:13 -0700

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Set the target URL
-URL="http://0.0.0.0:5000/catch_me"
-
-# Create a JSON file with the required content
-echo '{"key": "You got me!"}' > request.json
-
-# Send a POST request with the JSON file
-curl -s -X POST -H "Content-Type: application/json" -d @request.json "$URL" >/dev/null 2>&1
-
-# Clean up the temporary JSON file
-rm request.json
-
+# makes a req to 0.0.0.0:5000/catch_me for a response "You got me!"
+curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
