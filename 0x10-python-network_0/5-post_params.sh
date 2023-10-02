f81b889ee63ca9fa69934b0cbefdf4130e814017
starjakes <starjakese@gmail.com>
2023-10-01 19:54:26 -0700

    5. cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# # A Bash script that sends a POST request to a URL with a variables sent
-curl -s -d "starjakese@gmail.com" -d "subject=I will always be here for PLD" -X "POST" "$1
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
